6916dd2de9f30f9b09740825d2578a8b6030b6d1
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-10-07 20:23:20 +0700

    feat(zod): setup schemas and types for todo app

---
+++ src/features/todos/schemas/todo.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+
+export const TodoSchema = z.object({
+  id: z.uuid(),
+  task: z.string().min(1, 'Title is required'),
+  completed: z.boolean().default(false),
+})
+export const TodosSchema = z.array(TodoSchema)
+
+export type Todo = z.infer<typeof TodoSchema>
+export type Todos = Todo[]
+
+export const CreateTodoSchema = z.object({
+  title: z.string().min(1, 'Title is required'),
+})
+
+export type CreateTodoInput = z.infer<typeof CreateTodoSchema>
