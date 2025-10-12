3269ebd33fc0b94a26417f9d9b32c732781fd655
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-10-13 03:15:58 +0700

    feat(ui): place live char counter on top of the input instead for better UX

--- src/features/todos/components/AddTodoInput.tsx
+++ src/features/todos/components/AddTodoInput.tsx
@@ -28,30 +28,48 @@ const AddTodoInput = () => {
 
   return (
     <form onSubmit={handleAdd} className="flex flex-wrap gap-4 ">
-      <input
-        type="text"
-        placeholder="Add new task here"
-        onFocus={() => {
-          setTouched(true)
-          setFocused(true)
-        }}
-        onBlur={() => setFocused(false)}
-        className={[
-          'px-2 py-1 ring-1 ring-gray-400 rounded-md',
-          touched && newTodo.length > 20 && 'ring-2 ring-red-600',
-        ].join(' ')}
-        value={newTodo}
-        ref={inputRef}
-        onKeyDown={(e) => {
-          if (e.key === 'Escape') {
-            e.preventDefault()
-            requestAnimationFrame(() => {
-              inputRef.current?.blur()
-            })
-          }
-        }}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
+      <div className="relative">
+        <input
+          type="text"
+          placeholder="Add new task here"
+          onFocus={() => {
+            setTouched(true)
+            setFocused(true)
+          }}
+          onBlur={() => setFocused(false)}
+          className={[
+            'px-2 py-1 ring-1 ring-gray-400 rounded-md',
+            touched && newTodo.length > 20 && 'ring-2 ring-red-600',
+          ].join(' ')}
+          value={newTodo}
+          ref={inputRef}
+          onKeyDown={(e) => {
+            if (e.key === 'Escape') {
+              e.preventDefault()
+              requestAnimationFrame(() => {
+                inputRef.current?.blur()
+              })
+            }
+          }}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <span
+          className={[
+            'text-[0.7rem] tabular-nums text-gray-500 select-none',
+            'absolute right-2 -top-2 bg-white px-0.5',
+          ].join(' ')}
+        >
+          <span
+            className={[
+              touched && newTodo.length > 20 && 'text-red-400',
+              focused && newTodo.length === 0 && 'text-amber-400',
+            ].join(' ')}
+          >
+            {newTodo.length < 10 ? '0' + newTodo.length : newTodo.length}
+          </span>
+          |20
+        </span>
+      </div>
       <button
         className="btn bg-amber-400 disabled:opacity-40"
         type="submit"
@@ -59,17 +77,6 @@ const AddTodoInput = () => {
       >
         <IoMdAdd size={20} />
       </button>
-      <span className={['text-xs tabular-nums text-gray-500 '].join(' ')}>
-        <span
-          className={[
-            touched && newTodo.length > 20 && 'text-red-400',
-            focused && newTodo.length === 0 && 'text-amber-400',
-          ].join(' ')}
-        >
-          {newTodo.length < 10 ? '0' + newTodo.length : newTodo.length}
-        </span>
-        |20
-      </span>
     </form>
   )
 }
