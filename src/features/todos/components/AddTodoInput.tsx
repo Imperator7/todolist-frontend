6c96d022c7a1efe6390676c0422de46cd8adcfc0
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-10-13 03:01:54 +0700

    feat(ui): add live char counter and color ring indication for input validation

--- src/features/todos/components/AddTodoInput.tsx
+++ src/features/todos/components/AddTodoInput.tsx
@@ -4,26 +4,42 @@ import { IoMdAdd } from 'react-icons/io'
 
 const AddTodoInput = () => {
   const [newTodo, setNewTodo] = useState<string>('')
+  const [focused, setFocused] = useState<boolean>(false)
+  const [touched, setTouched] = useState<boolean>(false)
   const inputRef = useRef<HTMLInputElement>(null)
 
-  const { create } = useTodosActions()
+  const isValid = newTodo.length !== 0 && newTodo.length <= 20
+
+  const { create, isCreating } = useTodosActions()
 
   const handleAdd = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    requestAnimationFrame(() => {
-      inputRef.current?.focus()
-    })
-    if (newTodo.trim() === '') return
-    create(newTodo)
-    setNewTodo('')
+
+    if (!isValid) return
+
+    try {
+      await create(newTodo)
+      setNewTodo('')
+      setTouched(false)
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
-    <form onSubmit={handleAdd} className="flex gap-4">
+    <form onSubmit={handleAdd} className="flex flex-wrap gap-4 ">
       <input
         type="text"
         placeholder="Add new task here"
-        className="px-2 py-1 ring-1 ring-gray-200 rounded-md "
+        onFocus={() => {
+          setTouched(true)
+          setFocused(true)
+        }}
+        onBlur={() => setFocused(false)}
+        className={[
+          'px-2 py-1 ring-1 ring-gray-400 rounded-md',
+          touched && newTodo.length > 20 && 'ring-2 ring-red-600',
+        ].join(' ')}
         value={newTodo}
         ref={inputRef}
         onKeyDown={(e) => {
@@ -36,9 +52,24 @@ const AddTodoInput = () => {
         }}
         onChange={(e) => setNewTodo(e.target.value)}
       />
-      <button className="btn bg-amber-400" type="submit">
+      <button
+        className="btn bg-amber-400 disabled:opacity-40"
+        type="submit"
+        disabled={isCreating || !isValid}
+      >
         <IoMdAdd size={20} />
       </button>
+      <span className={['text-xs tabular-nums text-gray-500 '].join(' ')}>
+        <span
+          className={[
+            touched && newTodo.length > 20 && 'text-red-400',
+            focused && newTodo.length === 0 && 'text-amber-400',
+          ].join(' ')}
+        >
+          {newTodo.length < 10 ? '0' + newTodo.length : newTodo.length}
+        </span>
+        |20
+      </span>
     </form>
   )
 }
