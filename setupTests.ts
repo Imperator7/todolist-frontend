5bca0085ff45bf43f1c188860556a69e6d9ea253
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-11-04 14:23:23 +0700

    feat(test): mock requestAnimationFrame wih setTimeout for testability

--- setupTests.ts
+++ setupTests.ts
@@ -1,10 +1,15 @@
-import { beforeAll, afterEach, afterAll } from 'vitest'
+import { vi, beforeAll, afterEach, afterAll } from 'vitest'
 import '@testing-library/jest-dom'
 import { setupServer } from 'msw/node'
 import { handlers } from './src/mocks/handlers'
 
+global.requestAnimationFrame = (cb: FrameRequestCallback) => setTimeout(cb, 0)
+
 export const mockServer = setupServer(...handlers)
 
-beforeAll(() => mockServer.listen())
-afterEach(() => mockServer.resetHandlers())
+beforeAll(() => mockServer.listen({ onUnhandledRequest: 'error' }))
+afterEach(() => {
+  mockServer.resetHandlers()
+  vi.clearAllMocks()
+})
 afterAll(() => mockServer.close())
