5a616480360cb16dde64d6c4a05b03a0838273f4
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-11-05 13:29:21 +0700

    chore(todo): adjust ring around the todo component to make it distinguish from the background color

--- src/features/todos/layout/Todo.tsx
+++ src/features/todos/layout/Todo.tsx
@@ -4,7 +4,7 @@ import TodoHeader from '../components/TodoHeader'
 
 const Todo = () => {
   return (
-    <div className="flex flex-col mx-auto gap-4 p-4 bg-white/80 rounded-2xl w-full max-w-[800px] ring-1 ring-black/5">
+    <div className="flex flex-col mx-auto gap-4 p-4 bg-white/80 rounded-2xl w-full max-w-[800px] ring-1 ring-black/25">
       <TodoHeader />
       <AddTodoInput />
       <TodoList />
