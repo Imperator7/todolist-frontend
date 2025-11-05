01e444fd4c3326acc30f02a176d26002dde67741
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-11-05 17:43:32 +0700

    ref(header): add testid for task counter

--- src/features/todos/components/TodoHeader.tsx
+++ src/features/todos/components/TodoHeader.tsx
@@ -3,16 +3,19 @@ import { useTodoLength } from '../hooks/useTodoLength'
 const TodoHeader = () => {
   const { data: todosLength = 0 } = useTodoLength()
 
+  const label = todosLength === 1 ? 'task' : 'tasks'
+
   return (
     <h1 className="font-bold">
       <span>Task List</span>
       <span
+        data-testid="todo-counter"
         className={[
           'text-gray-500 caret-transparent',
           todosLength === 0 && 'hidden',
         ].join(' ')}
       >
-        : {todosLength} tasks
+        : {todosLength} {label}
       </span>
     </h1>
   )
