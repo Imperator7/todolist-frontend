eeb8aa3021ac6ed0ff728849ad833022a7be5575
Imperator7 <140708045+Imperator7@users.noreply.github.com>
2025-11-05 13:30:46 +0700

    ref(test): remove unnecessary setup for unit testing

--- setupTests.ts
+++ setupTests.ts
@@ -1,15 +1,8 @@
-import { vi, beforeAll, afterEach, afterAll } from 'vitest'
+import { vi, afterEach } from 'vitest'
 import '@testing-library/jest-dom'
-import { setupServer } from 'msw/node'
-import { handlers } from './src/mocks/handlers'
 
 global.requestAnimationFrame = (cb: FrameRequestCallback) => setTimeout(cb, 0)
 
-export const mockServer = setupServer(...handlers)
-
-beforeAll(() => mockServer.listen({ onUnhandledRequest: 'error' }))
 afterEach(() => {
-  mockServer.resetHandlers()
-  vi.clearAllMocks()
+  vi.resetAllMocks()
 })
-afterAll(() => mockServer.close())
